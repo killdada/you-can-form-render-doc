16bbec0d3c796fbf206dd98d862d0aceded7254b
yen <1124268308@qq.com>
2022-05-27 16:13:38 +0800

    docs: 重新提交

--- .umirc.ts
+++ .umirc.ts
@@ -5,14 +5,9 @@ export default defineConfig({
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  outputPath: 'docs-dist',
   mode: 'site',
   navs: [
     null,
     { title: 'Git', path: 'https://github.com/killdada/you-can-form-render' },
   ],
-  // webpack5: {},
-  // dynamicImport: {},
-  // mfsu: {},
-  // more config: https://d.umijs.org/config
 });
